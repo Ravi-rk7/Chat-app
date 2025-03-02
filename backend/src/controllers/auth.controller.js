ac7976a40b1563abaf713d1f57929416283e67f2
Ravikiran Tandale <coding.ravikirantan7@gmail.com>
2025-03-03 00:52:14 +0530

    Completed Update profile and protectRoute mw

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,7 +1,7 @@
 import { generateToken } from "../lib/utils.js";
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
-
+import cloudinary from "../lib/cloudinary.js";
 
 export const signup = async (req,res)=>{
     const {fullName,password,email} = req.body;
@@ -87,6 +87,37 @@ export const logout = (req,res)=>{
 
     } catch (error) {
         console.log(`Error in Logout Controller: ${error.message}`);
+        res.status(500).json({message:"Internal server error!"});
+    }
+}
+
+export const updateProfile = async (req,res)=>{
+
+    try {
+        
+        const {profilePic} = req.body;
+        const userId = req.user._id;
+
+        if(!profilePic){
+            return res.status(400).json({message:"Profile picture is required!"});
+        }
+
+        const uploadResponse = await cloudinary.uploader.upload(profilePic);
+        const updatedUser = await User.findByIdAndUpdate(userId,{profilePic:uploadResponse.secure_url},{new:true});
+
+        res.status(200).json(updatedUser);
+
+    } catch (error) {
+        console.log(`Error in update profile Controller: ${error.message}`);
+        res.status(500).json({message:"Internal server error!"});
+    }
+}
+
+export const checkAuth = (req,res)=>{
+    try {
+        return res.status(200).json(req.user);
+    } catch (error) {
+        console.log(`Error in checkAuth Controller: ${error.message}`);
         res.status(500).json({message:"Internal server error!"});
     }
 }
--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import {connectDB} from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 const app = express();
@@ -11,7 +12,7 @@ const PORT = process.env.PORT;
 app.use(express.json())
 
 app.use("/api/auth",authRoutes);
-
+app.use(cookieParser());
 app.listen(PORT,(req,res)=>{
     console.log(`app is running on port: ${PORT}....`);
     connectDB();
