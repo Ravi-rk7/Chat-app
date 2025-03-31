35e1a2bafbffbd1b72b0457a13ac3074f7f473ad
Ravikiran Tandale <coding.ravikirantan7@gmail.com>
2025-04-01 02:45:03 +0530

    Completed signup page

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,101 +1,97 @@
-import { Eye, EyeOff, Mail, MessageSquare ,Loader2,Lock, User} from "lucide-react";
-import React, { useState } from "react";
+import { useState } from "react";
 import { useAuthStore } from "../store/useAuthStore";
-import {Link} from "react-router-dom";
-import AuthImagePattern from "../components/AuthImagePattern.jsx";
+import { Eye, EyeOff, Loader2, Lock, Mail, MessageSquare, User } from "lucide-react";
+import { Link } from "react-router-dom";
+
+import AuthImagePattern from "../components/AuthImagePattern";
 import toast from "react-hot-toast";
 
 const SignUpPage = () => {
   const [showPassword, setShowPassword] = useState(false);
-  
+
   const [formData, setFormData] = useState({
-    fullname: "",
+    fullName: "",
     email: "",
     password: "",
   });
-  
+
   const { signup, isSigningUp } = useAuthStore();
 
   const validateForm = () => {
-    if (!formData.fullname.trim()) return toast.error("Full Name is required! ");
-    if (!formData.email.trim()) return toast.error("E-mail is required! ");
+    if (!formData.fullName.trim()) return toast.error("Full name is required");
+    if (!formData.email.trim()) return toast.error("Email is required");
     if (!/\S+@\S+\.\S+/.test(formData.email)) return toast.error("Invalid email format");
-    if (!formData.password.trim()) return toast.error("Password is required! ");
-    if (formData.password.length < 6) return toast.error("Password must be atleast 6 characters ! ");
-    
+    if (!formData.password) return toast.error("Password is required");
+    if (formData.password.length < 6) return toast.error("Password must be at least 6 characters");
+
     return true;
   };
-  
+
   const handleSubmit = (e) => {
     e.preventDefault();
 
     const success = validateForm();
 
-    if(success===true){
-      signup(formData);
-    }
+    if (success === true) signup(formData);
   };
 
   return (
     <div className="min-h-screen grid lg:grid-cols-2">
-      {/* Left Side */}
+      {/* left side */}
       <div className="flex flex-col justify-center items-center p-6 sm:p-12">
         <div className="w-full max-w-md space-y-8">
           {/* LOGO */}
           <div className="text-center mb-8">
             <div className="flex flex-col items-center gap-2 group">
               <div
-                className="size-12 rounded-xl bg-primary/10 flex items-center justify-center
-                  group-hover:bg-primary/20 transition-colors"
+                className="size-12 rounded-xl bg-primary/10 flex items-center justify-center 
+              group-hover:bg-primary/20 transition-colors"
               >
                 <MessageSquare className="size-6 text-primary" />
               </div>
-              <h1 className="text-2xl font-bold mt-2 ">Create Account</h1>
-              <p className=" text-base-content/60">
-                Get started with your free account
-              </p>
+              <h1 className="text-2xl font-bold mt-2">Create Account</h1>
+              <p className="text-base-content/60">Get started with your free account</p>
             </div>
           </div>
 
           <form onSubmit={handleSubmit} className="space-y-6">
          
    {/* Full Name */}
+
             <div className="form-control">
               <label className="label">
                 <span className="label-text font-medium">Full Name</span>
               </label>
               <div className="relative">
                 <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                  <User className="size-5 text-base-content/40" />
+                <User className="w-5 h-5 text-base-content/40 z-10" />
                 </div>
                 <input
                   type="text"
-                  className={"input input-bordered w-full pl-10"}
+                  className={`input input-bordered w-full pl-10`}
                   placeholder="John Doe"
-                  value={formData.fullname}
-                  onChange={(e) =>
-                    setFormData({ ...formData, fullname: e.target.value })
-                  }
+                  value={formData.fullName}
+                  onChange={(e) => setFormData({ ...formData, fullName: e.target.value })}
                 />
               </div>
             </div>
+
 {/* Email */}
+
             <div className="form-control">
               <label className="label">
                 <span className="label-text font-medium">Email</span>
               </label>
               <div className="relative">
                 <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                  <Mail className="size-5 text-base-content/40" />
+                  <Mail className="size-5 text-base-content/40 z-10" />
                 </div>
                 <input
                   type="email"
-                  className={"input input-bordered w-full pl-10"}
-                  placeholder="you@gmail.com"
+                  className={`input input-bordered w-full pl-10`}
+                  placeholder="you@example.com"
                   value={formData.email}
-                  onChange={(e) =>
-                    setFormData({ ...formData, email: e.target.value })
-                  }
+                  onChange={(e) => setFormData({ ...formData, email: e.target.value })}
                 />
               </div>
             </div>
@@ -108,31 +104,30 @@ const SignUpPage = () => {
               </label>
               <div className="relative">
                 <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                  <Lock className="size-5 text-base-content/40" />
+                  <Lock className="size-5 text-base-content/40 z-10" />
                 </div>
                 <input
                   type={showPassword ? "text" : "password"}
-                  className={"input input-bordered w-full pl-10"}
-                  placeholder=""
+                  className={`input input-bordered w-full pl-10`}
+                  placeholder="••••••••"
                   value={formData.password}
-                  onChange={(e) =>
-                    setFormData({ ...formData, password: e.target.value })
-                  }
+                  onChange={(e) => setFormData({ ...formData, password: e.target.value })}
                 />
-                <button 
-                type="button"
-                className="absolute inset-y-0 right-0 pr-3 flex items-center"
-                onClick={()=>{setShowPassword(!showPassword)}}
+                <button
+                  type="button"
+                  className="absolute inset-y-0 right-0 pr-3 flex items-center"
+                  onClick={() => setShowPassword(!showPassword)}
                 >
-                  {showPassword ? (<EyeOff className="size-5 text-base-content/40" />
-                ) : ( 
+                  {showPassword ? (
+                    <EyeOff className="size-5 text-base-content/40" />
+                  ) : (
                     <Eye className="size-5 text-base-content/40" />
-                    )}
+                  )}
                 </button>
               </div>
             </div>
 
-    {/* Submit Button */}
+{/* Submit Button */}
 
             <button type="submit" className="btn btn-primary w-full" disabled={isSigningUp}>
               {isSigningUp ? (
@@ -144,7 +139,6 @@ const SignUpPage = () => {
                 "Create Account"
               )}
             </button>
-
           </form>
 
           <div className="text-center">
@@ -155,19 +149,16 @@ const SignUpPage = () => {
               </Link>
             </p>
           </div>
-
         </div>
       </div>
 
-{/* Right Side */}
+      {/* right side */}
 
-        <AuthImagePattern 
-          title="Join Our Community"
-          subtitle = "Connect with friends, share moments, and stay in touch with your loved ones."
-        />
-
+      <AuthImagePattern
+        title="Join our community"
+        subtitle="Connect with friends, share moments, and stay in touch with your loved ones."
+      />
     </div>
   );
 };
-
 export default SignUpPage;
